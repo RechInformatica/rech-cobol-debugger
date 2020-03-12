86a90e0fd78123de47188cc491716b85d7f9a305
Gustavo Cassel <guscassel@gmail.com>
2020-03-12 09:07:55 -0300

    Now debug console supports simple breakpoints when user specifies line and program

--- src/DebuggerReplManager.ts
+++ src/DebuggerReplManager.ts
@@ -1,4 +1,9 @@
 import { DebugInterface } from "./debugProcess/DebugInterface";
+import { CobolBreakpoint } from "./debugProcess/DebugBreakpoint";
+import { debug, Uri, Position, Location, SourceBreakpoint } from 'vscode';
+
+/** Breakpoint commands for debug console */
+const DEBUGGER_BREAK_COMMANDS = ["br"];
 
 /**
  * Class to manage 'repl' (Read–eval–print loop) console commands
@@ -23,7 +28,50 @@ export class DebuggerReplManager {
 	 * @param command
 	 */
 	public handleCommand(command: string): void {
-		this.debugRuntime.sendRawCommand(command);
+		if (this.isBreakCommand(command)) {
+			const cobolBreak = this.extractBreakFromCmd(command);
+			if (cobolBreak) {
+				const uri = Uri.file(cobolBreak.source);
+				const pos = new Position(cobolBreak.line - 1, 0);
+				const loc = new Location(uri, pos);
+				const breakpoint = new SourceBreakpoint(loc, true);
+				debug.addBreakpoints([breakpoint]);
+			} else {
+				this.debugRuntime.sendRawCommand(command);
+			}
+		} else {
+			this.debugRuntime.sendRawCommand(command);
+		}
+	}
+
+	/**
+	 * Checks if user has typed a breakpoint command on Debug console
+	 *
+	 * @param command command to check
+	 */
+	private isBreakCommand(command: string): boolean {
+		const splittedCommand = command.toLowerCase().trim().split(/\s+/);
+		return DEBUGGER_BREAK_COMMANDS.some(b => splittedCommand[0].startsWith(b));
+	}
+
+	/**
+	 * Extracts a CobolBreakpoint from the command specified on current line
+	 *
+	 * @param command command line typed by user on Debug Console
+	 */
+	private extractBreakFromCmd(command: string): CobolBreakpoint | undefined {
+		const splitted = command.trim().split(/\s+/);
+		if (splitted.length > 2) {
+			const line = Number(splitted[1]);
+			const source = splitted[2];
+			if (!isNaN(line)) {
+				return {
+					line: line,
+					source: source,
+				}
+			}
+		}
+		return undefined;
 	}
 
 }
