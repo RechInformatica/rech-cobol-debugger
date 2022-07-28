4fd2c8df00955d58d22425719f197cfd0e03f31e
Vanduir <vanduir@rech.com.br>
2022-07-28 16:19:23 -0300

    Adjusted to call externar path resolver if need

--- src/debugProcess/RequestAvailableSourceDirectoriesCommand.ts
+++ src/debugProcess/RequestAvailableSourceDirectoriesCommand.ts
@@ -26,7 +26,7 @@ export class RequestAvailableSourceDirectoriesCommand implements DebugCommand<vo
 		if (match && match.groups) {
 			const groups = match.groups;
 			const directories = groups[AVAILABLE_DIRECTORIES_NAMED_GROUP];
-			const splitted = directories.split(';');
+			const splitted = directories.split(/;|:/);
 			return splitted;
 		}
 	}
